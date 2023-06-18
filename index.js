edd20e6c08255962828b696a6e232d2baaa4996a
Abdul Arham <abdul.arham9123@gmail.com>
2023-06-18 18:52:57 +0500

    edited index.js

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const connectToMongo = require("./utils/db");
+
+app.use(bodyParser.json())
+
+connectToMongo()
+
+app.use(cors())
+
+app.use("/users", require("./routes/users"))
 
 app.get("/", (req, res)=>{
     res.send("Hello World");
